b4efc51d529fd7a794c2698ad1696115bd9d7131
madongming3 <39984857+madongming3@users.noreply.github.com>
2019-03-25 20:57:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
-cmds:
-  - name: 'list current dir'
-    cmd: 'ls'
-  - name: 'make output dir'
-    cmd: 'mkdir -p output'
-  - name: 'copy to output dir'
-    cmd: 'cp -r config console data function install lib static templates tmp web config.php db.cfg.php function.php index.php start.sh appSpec.yml output'
+#!/bin/bash
+set -x
+env
 
-# 抽包路径, 这个是必选项
-out_dir: 'output'
+rm -fr output
+mkdir output
+cp -fr config console data function install lib static templates tmp web config.php db.cfg.php function.php index.php start.sh appSpec.yml output/
