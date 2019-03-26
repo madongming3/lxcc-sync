1f3db17d1ce93b489703ea3675faa1e3c0fb004e
madongming3 <39984857+madongming3@users.noreply.github.com>
2019-03-26 14:50:06 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ env
 
 rm -fr output
 mkdir output
-cp -fr config console data function install lib static templates tmp web config.php db.cfg.php function.php index.php start.sh appSpec.yml output/
+cp -fr config console data function install lib static templates tmp web config.php db.cfg.php function.php index.php start.sh  output/
