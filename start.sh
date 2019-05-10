5ff550f4d6d1150e7c8f771610924d9735cd4a0d
madongming3 <39984857+madongming3@users.noreply.github.com>
2019-05-10 16:40:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
 if [ "$sip" == "192.168.0.68" ];then
-    exit 1
+ echo 'hello world'
 #fi
 #if [ "$sip" == "192.168.0.66" ];then
  #   exit 1
